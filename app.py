90a8536152c77efbc798d0b51d861ecbb9fcd6ea
RabinSubedi3180 <rabin.subedi135@gmail.com>
2026-07-03 16:26:28 +0545

    Initail Commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+	env = os.getenv("APP_ENV", "Development")
+	return f"<h1>Hello from DevOps Pipeline! Target Env: {env}</h1>"
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=5000)
