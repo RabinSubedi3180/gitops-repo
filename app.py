d99df0547b301881a86bae794799522755862eb9
RabinSubedi3180 <rabin.subedi135@gmail.com>
2026-07-04 11:33:18 +0545

    Feat: update homepage message

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
 	env = os.getenv("APP_ENV", "Development")
-	return f"<h1>Hello from DevOps Pipeline! Target Env: {env}</h1>"
+	return f"<h1>Hello from Updated DevOps Pipeline! Target Env: {env}</h1>"
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
